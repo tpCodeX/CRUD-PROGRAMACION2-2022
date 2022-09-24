c1d1f78cd69d9d603b45995a47286bff2e01a8b2
tpcodex <ponce.code.mza@gmail.com>
2022-09-24 04:27:25 -0300

    modificación de script par que sea retornable desde todas las vistas y acortacion del tiempo de espera a 2seg

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 function counter() {
-  let countdown = 3;
+  let countdown = 2;
 
   let timerDiv = document.getElementById("timer");
 
@@ -8,7 +8,7 @@ function counter() {
     countdown--;
     if (countdown === 0) {
       clearInterval(timer);
-      window.location.href = "./"
+      window.location.href = "../"
     }
   }, 1000);
 }
