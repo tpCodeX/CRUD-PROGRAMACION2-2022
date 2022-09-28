2dae0e7faf23b9d8524f136ecd19e3eccf2b9a52
tpCodeX <ponce.code.mza@gmail.com>
2022-09-28 02:40:31 -0300

    modificacion del script para redireccionar. Ahora redirecciona a una ruta relativa según lso registros que estemos observando

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,16 @@
 function counter() {
   let countdown = 2;
-
+  var pathActual = window.location.href
+  pathActual=pathActual.split("/")
+  var pathRelativa=pathActual[0]+"//"+pathActual[2]+"/"+pathActual[3]
   let timerDiv = document.getElementById("timer");
 
   let timer = setInterval(function () {
-    timerDiv.innerHTML = `Regresando a Home: ${countdown}`;
+    timerDiv.innerHTML = `Regresando a Home: ${countdown} `;
     countdown--;
     if (countdown === 0) {
       clearInterval(timer);
-      window.location.href = "../"
+      window.location.href = pathRelativa
     }
   }, 1000);
 }
